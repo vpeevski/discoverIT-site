8bb745349e50cbb68eb057ca78e5ae2e96c76b07
Vasil Peevski <vassilpeevsky@gmail.com>
2016-07-02 08:58:55 +0300

    WrongEmail test with single method

--- src/discoverita/WrongEmail.java
+++ src/discoverita/WrongEmail.java
@@ -14,56 +14,18 @@ public class WrongEmail {
 	}
 			
 	public static void checkBlankJava() {
-		WebElement nameField = BrowserFirefox.driver.findElement(By.id("firstname_id"));
-		nameField.sendKeys(RandomStringGen.randomStringAlphaNumericNotEmpty());
-		WebElement familyNameField = BrowserFirefox.driver.findElement(By.id("lastname_id"));
-		familyNameField.sendKeys(RandomStringGen.randomStringAlphaNumericNotEmpty());
-		
-		WebElement selectCourseButton = BrowserFirefox.driver.findElement(By.id("course_id"));
-		selectCourseButton.click();
-		BrowserFirefox.driver.findElement(By.id("course_id")).sendKeys("Java за начинаещи");
-		
-		WebElement eMailField = BrowserFirefox.driver.findElement(By.id("email_id"));
-		eMailField.sendKeys(RandomStringGen.randomStringAlphaNumericNotEmpty());
-		
-		WebElement contactTypeButton = BrowserFirefox.driver.findElement(By.id("contact_type_id"));
-		contactTypeButton.click();
-		BrowserFirefox.driver.findElement(By.id("contact_type_id")).sendKeys("Се запиша");
-		
-		WebElement submitButton = BrowserFirefox.driver.findElement(By.xpath("//input[@name='send']"));
-		submitButton.click();
-		
-		WebElement message = BrowserFirefox.driver.findElement(By.xpath("//span[@id='email_label_group']//span[@class='small-text help-block']"));
-		String observedText = message.getText();
-		assertEquals("* невалиден e-mail", observedText);
+		checkBlank ("Java за начинаещи");
 	}
 	
 	public static void checkBlankDataBase() {
-		WebElement nameField = BrowserFirefox.driver.findElement(By.id("firstname_id"));
-		nameField.sendKeys(RandomStringGen.randomStringAlphaNumericNotEmpty());
-		WebElement familyNameField = BrowserFirefox.driver.findElement(By.id("lastname_id"));
-		familyNameField.sendKeys(RandomStringGen.randomStringAlphabetNotEmpty());
-		
-		WebElement selectCourseButton = BrowserFirefox.driver.findElement(By.id("course_id"));
-		selectCourseButton.click();
-		BrowserFirefox.driver.findElement(By.id("course_id")).sendKeys("Бази данни");
-		
-		WebElement eMailField = BrowserFirefox.driver.findElement(By.id("email_id"));
-		eMailField.sendKeys(RandomStringGen.randomStringAlphaNumericNotEmpty());
-		
-		WebElement contactTypeButton = BrowserFirefox.driver.findElement(By.id("contact_type_id"));
-		contactTypeButton.click();
-		BrowserFirefox.driver.findElement(By.id("contact_type_id")).sendKeys("Се запиша");
-		
-		WebElement submitButton = BrowserFirefox.driver.findElement(By.xpath("//input[@name='send']"));
-		submitButton.click();
-		
-		WebElement message = BrowserFirefox.driver.findElement(By.xpath("//span[@id='email_label_group']//span[@class='small-text help-block']"));
-		String observedText = message.getText();
-		assertEquals("* невалиден e-mail", observedText);
+		checkBlank ("Бази данни");
 	}
 	
 	public static void checkBlankWebDesign() {
+		checkBlank ("Web design (HTML5, CSS3, JavaScript)");
+	}
+	
+	private static void checkBlank (String courseSelected) {
 		WebElement nameField = BrowserFirefox.driver.findElement(By.id("firstname_id"));
 		nameField.sendKeys(RandomStringGen.randomStringAllCharsNotEmpty());
 		WebElement familyNameField = BrowserFirefox.driver.findElement(By.id("lastname_id"));
@@ -71,7 +33,7 @@ public class WrongEmail {
 		
 		WebElement selectCourseButton = BrowserFirefox.driver.findElement(By.id("course_id"));
 		selectCourseButton.click();
-		BrowserFirefox.driver.findElement(By.id("course_id")).sendKeys("Web design (HTML5, CSS3, JavaScript)");
+		BrowserFirefox.driver.findElement(By.id("course_id")).sendKeys(courseSelected);
 		
 		WebElement eMailField = BrowserFirefox.driver.findElement(By.id("email_id"));
 		eMailField.sendKeys(RandomStringGen.randomStringAlphaNumericNotEmpty());
